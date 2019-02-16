abb5da305c76768a34563c645e550dd8e5080a34
qalmee <komarevtsev24@gmail.com>
2019-02-16 19:59:07 +0700

    Конвертер с P'ичных на 10'тичные вроде работает

--- src/main/java/stp/Main.java
+++ src/main/java/stp/Main.java
@@ -1,13 +1,16 @@
 package stp;
 
 import stp.converter.Converter10ToP;
+import stp.converter.ConverterPTo10;
 
 import java.math.BigDecimal;
 
 public class Main {
 
     public static void main(String[] args) {
-        String s = Converter10ToP.convert(new BigDecimal("254.1021"), 2, 100);
+        String s = Converter10ToP.convert("122", 2, 99);
+        String s1 = ConverterPTo10.convert("0", 16, 0);
         System.out.println(s);
+        System.out.println(s1);
     }
 }
