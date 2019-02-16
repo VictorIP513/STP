2d9027786fa14f5e06df0a22106b44e23aeacca4
qalmee <komarevtsev24@gmail.com>
2019-02-16 21:09:41 +0700

    багуля

--- src/main/java/stp/Main.java
+++ src/main/java/stp/Main.java
@@ -8,8 +8,8 @@ import java.math.BigDecimal;
 public class Main {
 
     public static void main(String[] args) {
-        String s = Converter10ToP.convert("122", 2, 99);
-        String s1 = ConverterPTo10.convert("0", 16, 0);
+        String s = Converter10ToP.convert("-238957209.02389572090", 12, 15);
+        String s1 = ConverterPTo10.convert("-238957209.02389572090", 12, 15);
         System.out.println(s);
         System.out.println(s1);
     }
