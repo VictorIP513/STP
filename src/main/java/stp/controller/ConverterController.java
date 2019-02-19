89298f43f3213ec2d768d2e230a645d960bf6f52
VictorIP513 <VictorIP513@users.noreply.github.com>
2019-02-19 21:59:34 +0700

    ui: spinners and sliders

--- src/main/java/stp/controller/ConverterController.java
+++ src/main/java/stp/controller/ConverterController.java
@@ -1,4 +1,40 @@
 package stp.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
+
 public class ConverterController {
+
+    @FXML
+    private Spinner<Integer> spinnerInputBase;
+
+    @FXML
+    private Spinner<Integer> spinnerOutputBase;
+
+    @FXML
+    private Slider sliderInputBase;
+
+    @FXML
+    private Slider sliderOutputBase;
+
+    @FXML
+    public void initialize() {
+        configureSpinners();
+        configureSliders();
+    }
+
+    private void configureSpinners() {
+        spinnerInputBase.valueProperty().addListener(
+                (observable, oldValue, newValue) -> sliderInputBase.setValue(newValue));
+        spinnerOutputBase.valueProperty().addListener(
+                (observable, oldValue, newValue) -> sliderOutputBase.setValue(newValue));
+    }
+
+    private void configureSliders() {
+        sliderInputBase.valueProperty().addListener(
+                (observable, oldValue, newValue) -> spinnerInputBase.getValueFactory().setValue(newValue.intValue()));
+        sliderOutputBase.valueProperty().addListener(
+                (observable, oldValue, newValue) -> spinnerOutputBase.getValueFactory().setValue(newValue.intValue()));
+    }
 }
